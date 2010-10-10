3b35deb6cbe605ae3480e697ea62e62164d4bbe8
Max Cutler <max@maxcutler.com>
2010-10-10 15:20:00 -0400

    Added FieldMaps to handle conversions to and from XML-RPC structs.

--- wordpress_xmlrpc/wordpress.py
+++ wordpress_xmlrpc/wordpress.py
@@ -1,197 +1,145 @@
 import xmlrpclib
 
+class FieldMap(object):
+	"""
+	Container for settings mapping a WordPress XML-RPC request/response struct
+	to a Python, programmer-friendly class. 
+	"""
+	def __init__(self, inputName, outputNames=None, default=None, conversion=None):
+		self.name = inputName
+		self.output_names = outputNames or [inputName]
+		self.default = default
+		self.conversion = conversion
+
 class WordPressBase(object):
+	"""
+	Base class for representing a WordPress object. Handles conversion
+	of an XML-RPC response to an object, and construction of a `struct`
+	to use in XML-RPC requests.
+
+	Child classes should define a `definition` property that contains
+	the list of fields and a `FieldMap` instance to handle conversion
+	for XML-RPC calls.
+	"""
+	def __init__(self, xmlrpc=None):
+		if self.definition:
+			self._def = {}
+			for key, value in self.definition.items():
+				if isinstance(value, FieldMap):
+					self._def[key] = value
+				else:
+					self._def[key] = FieldMap(value)
+
+		if xmlrpc and self._def:
+			for var, fmap in self._def.items():
+				setattr(self, var, xmlrpc.get(fmap.name, fmap.default))
+
+	@property
+	def struct(self):
+		data = {}
+		for var, fmap in self._def.items():
+			if hasattr(self, var):
+				for output in fmap.output_names:
+					if fmap.conversion:
+						data[output] = fmap.conversion(getattr(self, var))
+					else:
+						data[output] = getattr(self, var)
+		return data
+
 	def __repr__(self):
 		return '<%s: %s>' % (self.__class__.__name__, str(self))
 
 class WordPressPost(WordPressBase):
-    def __init__(self, xmlrpc=None):
-        if xmlrpc:
-            self.id = xmlrpc['postid']
-            self.user = xmlrpc['userid']
-            self.date_created = xmlrpc['dateCreated']
-            self.slug = xmlrpc['wp_slug']
-            self.post_status = xmlrpc['post_status']
-            self.title = xmlrpc['title']
-            self.description = xmlrpc['description']
-            self.excerpt = xmlrpc['mt_excerpt']
-            self.extended_text = xmlrpc['mt_text_more']
-            self.link = xmlrpc['link']
-            self.permalink = xmlrpc['permaLink']
-            self.allow_comments = xmlrpc['mt_allow_comments'] == 1
-            self.allow_pings = xmlrpc['mt_allow_pings'] == 1
-            self.tags = xmlrpc['mt_keywords']
-            self.categories = xmlrpc['categories']
-            self.custom_fields = xmlrpc['custom_fields']
-        else:
-            self.id = None
-            self.user = None
-            self.date_created = None
-            self.slug = ''
-            self.post_status = ''
-            self.title = ''
-            self.description = ''
-            self.excerpt = ''
-            self.extended_text = ''
-            self.link = ''
-            self.permalink = ''
-            self.allow_comments = False
-            self.allow_pings = False
-            self.tags = ''
-            self.categories = ['Uncategorized']
-            self.custom_fields = []
+	definition = {
+		'id': 'postid',
+		'user': 'wp_author_id',
+		'date_created': FieldMap('dateCreated', conversion=xmlrpclib.DateTime),
+		'slug': 'wp_slug',
+		'post_status': 'post_status',
+		'title': 'title',
+		'description': 'description',
+		'excerpt': 'mt_excerpt',
+		'extended_text': 'mt_text_more',
+		'link': 'link',
+		'permalink': 'permaLink',
+		'allow_comments': FieldMap('mt_allow_comments', conversion=int),
+		'allow_pings': FieldMap('mt_allow_pings', conversion=int),
+		'tags': 'mt_keywords',
+		'categories': 'categories',
+		'custom_fields': 'custom_fields',
+		'post_type': FieldMap('post_type', default='post'),
+	}
 
-    @property
-    def content_struct(self):
-        struct = {
-            'post_type': 'post',
-            'wp_author_id': self.user,
-            'title': self.title,
-            'description': self.description,
-            'mt_excerpt': self.excerpt,
-            'mt_text_more': self.extended_text,
-            'mt_keywords': self.tags,
-            'mt_allow_comments': int(self.allow_comments),
-            'mt_allow_pings': int(self.allow_pings),
-            'categories': self.categories,
-        }
-
-        if self.post_status:
-            struct['post_status'] = self.post_status
-
-        if self.date_created:
-            struct['dateCreated'] = xmlrpclib.DateTime(self.date_created)
-
-        if self.slug:
-            struct['wp_slug'] = self.slug
-
-        if self.custom_fields:
-            struct['custom_fields'] = self.custom_fields
-
-        return struct
-
-    def __str__(self):
-        return '%s (id=%s)' % (self.slug, self.id)
+	def __str__(self):
+		return '%s (id=%s)' % (self.slug, self.id)
 
 class WordPressBlog(WordPressBase):
-	id = None
-	name = None
-	url = ''
-	xmlrpc = ''
-	is_admin = False
-
-	def __init__(self, xmlrpc=None):
-		if xmlrpc:
-			self.id = xmlrpc['blogid']
-			self.name = xmlrpc['blogName']
-			self.url = xmlrpc['url']
-			self.xmlrpc = xmlrpc['xmlrpc']
-			self.is_admin = xmlrpc['isAdmin']
+	definition = {
+		'id': 'blogid',
+		'name': 'blogName',
+		'url': 'url',
+		'xmlrpc': 'xmlrpc',
+		'is_admin': FieldMap('isAdmin', default=False)
+	}
 
 	def __str__(self):
 		return self.name
 
 class WordPressAuthor(WordPressBase):
-	user_id = None
-	user_login = ''
-	display_name = ''
-
-	def __init__(self, xmlrpc=None):
-		if xmlrpc:
-			self.user_id = xmlrpc['user_id']
-			self.user_login = xmlrpc['user_login']
-			self.display_name = xmlrpc['display_name']
+	definition = {
+		'user_id': 'user_id',
+		'user_login': 'user_login',
+		'display_name': 'display_name',
+	}
 
 	def __str__(self):
 		return self.display_name
 
 class WordPressUser(WordPressBase):
-	user_id = None
-	nickname = ''
-	url = ''
-	first_name = ''
-	last_name = ''
-
-	def __init__(self, xmlrpc=None):
-		if xmlrpc:
-			self.user_id = xmlrpc['userid']
-			self.nickname = xmlrpc['nickname']
-			self.url = xmlrpc['url']
-			self.first_name = xmlrpc['firstname']
-			self.last_name = xmlrpc['lastname']
+	definition = {
+		'user_id': 'userid',
+		'nickname': 'nickname',
+		'url': 'url',
+		'first_name': 'firstname',
+		'last_name': 'lastname',
+	}
 
 	def __str__(self):
 		return self.nickname
 
 class WordPressCategory(WordPressBase):
-	cat_id = None
-	parent_id = None
-	name = ''
-	description = ''
-	url = ''
-	rss = ''
-
-	def __init__(self, xmlrpc=None):
-		if xmlrpc:
-			self.cat_id = xmlrpc.get('categoryId', None)
-			self.parent_id = xmlrpc.get('parentId', None)
-			self.name = xmlrpc.get('categoryName', '')
-			self.description = xmlrpc.get('categoryDescription', '')
-			self.url = xmlrpc.get('htmlUrl', '')
-			self.rss = xmlrpc.get('rssUrl', '')
-
-	@property
-	def content_struct(self):
-		struct = {
-			'name': self.name,
-		}
-
-		if self.cat_id:
-			struct['categoryId'] = self.cat_id
-
-		if self.parent_id:
-			struct['parent_id'] = self.parent_id
-
-		if self.description:
-			struct['description'] = self.description
-
-		return struct
+	definition = {
+		'cat_id': 'categoryId',
+		'parent_id': 'parentId',
+		'name': FieldMap('categoryName', ['categoryName', 'name']),
+		'description': 'categoryDescription',
+		'url': 'htmlUrl',
+		'rss': 'rssUrl',
+	}
 
 	def __str__(self):
 		return self.name
 
 class WordPressTag(WordPressBase):
-	tag_id = None
-	name = ''
-	count = 0
-	slug = ''
-	url = ''
-	rss = ''
-
-	def __init__(self, xmlrpc=None):
-		if xmlrpc:
-			self.tag_id = xmlrpc['tag_id']
-			self.name = xmlrpc['name']
-			self.count = xmlrpc['count']
-			self.slug = xmlrpc['slug']
-			self.url = xmlrpc['html_url']
-			self.rss = xmlrpc['rss_url']
+	definition = {
+		'tag_id': 'tag_id',
+		'name': 'name',
+		'count': 'count',
+		'slug': 'slug',
+		'url': 'html_url',
+		'rss': 'rss_url',
+	}
 
 	def __str__(self):
 		return self.name
 
 class WordPressOption(WordPressBase):
-	name = ''
-	description = ''
-	value = None
-	read_only = False
-
-	def __init__(self, xmlrpc=None):
-		if xmlrpc:
-			self.name = xmlrpc['name']
-			self.description = xmlrpc['desc']
-			self.value = xmlrpc['value']
-			self.read_only = xmlrpc['readonly']
+	definition = {
+		'name': 'name',
+		'description': 'desc',
+		'value': 'value',
+		'read_only': FieldMap('readonly', default=False),
+	}
 
 	def __str__(self):
 		return '%s="%s"' % (self.name, self.value)
