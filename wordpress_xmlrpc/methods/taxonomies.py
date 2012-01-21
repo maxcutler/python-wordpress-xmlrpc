4f1aa4efc4b30951bc333ba7cbee3e4d64478027
Max Cutler <max@maxcutler.com>
2012-01-21 14:09:23 -0500

    Implemented wp.getPostTerms and wp.setPostTerms.

--- wordpress_xmlrpc/methods/taxonomies.py
+++ wordpress_xmlrpc/methods/taxonomies.py
@@ -98,3 +98,54 @@ class DeleteTerm(AuthenticatedMethod):
     """
     method_name = 'wp.deleteTerm'
     method_args = ('taxonomy_name', 'term_id')
+
+
+class GetPostTerms(AuthenticatedMethod):
+    """
+    Retrieve list of terms assigned to a blog post.
+
+    Parameters:
+        `post_id`: ID of the blog post
+        `group_by_taxonomy`: `bool` specifying whether to return terms grouped by taxonomy name
+                             in a `dict` or as a flat list of `WordPressTerm`s
+
+    Returns: Depends on value of `group_by_taxonomy`:
+                True: `dict`, with taxonomy names as keys and `list` of `WordPressTerm` instances as values.
+                False: `list` of `WordPressTerm` instances.
+    """
+    method_name = 'wp.getPostTerms'
+    method_args = ('post_id', 'group_by_taxonomy')
+    results_class = WordPressTerm
+
+    def process_result(self, raw_result):
+        if not self.group_by_taxonomy:
+            # flat list, so normal processing
+            return super(GetPostTerms, self).process_result(raw_result)
+        else:
+            # dictionary of taxonomy/terms. process each value individually
+            result = {}
+            for taxonomy, terms in raw_result.items():
+                result[taxonomy] = super(GetPostTerms, self).process_result(terms)
+            return result
+
+
+class SetPostTerms(AuthenticatedMethod):
+    """
+    Assign a set of terms to a blog post.
+
+    Parameters:
+        `post_id`: ID of the blog post
+        `terms`: `dict` with taxonomy names as keys and `list` of term IDs as values
+
+    Returns: `True` on successful category assignment.
+
+    Example:
+        >>> client.call(SetPostTerms(15, {
+                'category': [1, 4, 199],
+                'post_tag': [6, 21, 39],
+                'custom_tax': [384]
+            }))
+        True
+    """
+    method_name = 'wp.setPostTerms'
+    method_args = ('post_id', 'terms',)
