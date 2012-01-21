4b46f9303c76f59a118d31150b54dde3a894616b
Max Cutler <max@maxcutler.com>
2012-01-21 15:25:02 -0500

    Added support for optional method arguments. (cherry picked from commit cb99c3ebf6be586571ad82756201953b1691bd00)

--- wordpress_xmlrpc/base.py
+++ wordpress_xmlrpc/base.py
@@ -52,7 +52,8 @@ class XmlrpcMethod(object):
 
     Properties:
         `method_name`: XML-RPC method name (e.g., 'wp.getUserInfo')
-        `method_args`: Tuple of method-specific parameters
+        `method_args`: Tuple of method-specific required parameters
+        `optional_args`: Tuple of method-specific optional parameters
         `args_start_position`: If greater than zero, this many dummy arguments will pad the beginning of the method argument list.
         `default_args_position`: The index in the `method_args` list at which the default arguments should be inserted.
         `results_class`: Python class which will convert an XML-RPC response dict into an object
@@ -63,18 +64,30 @@ class XmlrpcMethod(object):
         `process_results`: Performs actions on the raw result from the XML-RPC response.
     """
     method_name = None
-    method_args = None
+    method_args = tuple()
+    optional_args = tuple()
     args_start_position = 0
     default_args_position = 0
     results_class = None
 
     def __init__(self, *args):
-        if self.method_args:
-            if len(args) != len(self.method_args):
-                raise ValueError("Invalid number of parameters to %s" % self.method_name)
+        if self.method_args or self.optional_args:
+            if self.optional_args:
+                max_num_args = len(self.method_args) + len(self.optional_args)
+                if not ( len(self.method_args) <= len(args) <= max_num_args ):
+                    raise ValueError("Invalid number of parameters to %s" % self.method_name)
+            else:
+                if len(args) != len(self.method_args):
+                    raise ValueError("Invalid number of parameters to %s" % self.method_name)
 
             for i, arg_name in enumerate(self.method_args):
                 setattr(self, arg_name, args[i])
+
+            if self.optional_args:
+                for i, arg_name in enumerate(self.optional_args, start=len(self.method_args)):
+                    if i >= len(args):
+                        break
+                    setattr(self, arg_name, args[i])
 
     def default_args(self, client):
         """
@@ -90,14 +103,16 @@ class XmlrpcMethod(object):
         """
         default_args = self.default_args(client)
 
-        if self.method_args:
+        if self.method_args or self.optional_args:
+            optional_args = getattr(self, 'optional_args', tuple())
             args = []
-            for arg in self.method_args:
-                obj = getattr(self, arg)
-                if hasattr(obj, 'struct'):
-                    args.append(obj.struct)
-                else:
-                    args.append(obj)
+            for arg in (self.method_args + optional_args):
+                if hasattr(self, arg):
+                    obj = getattr(self, arg)
+                    if hasattr(obj, 'struct'):
+                        args.append(obj.struct)
+                    else:
+                        args.append(obj)
             args = args[:self.default_args_position] + list(default_args) + args[self.default_args_position:]
         else:
             args = default_args
